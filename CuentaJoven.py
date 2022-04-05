f5651bbcf13f53ff14145689bffecfd6cfb99157
TheTankyOne <61328349+TheTankyOne@users.noreply.github.com>
2022-04-04 21:09:20 -0400

    Update CuentaJoven.py

--- CuentaJoven.py
+++ CuentaJoven.py
@@ -1,6 +1,6 @@
 from Cuenta import Cuenta
 
 class Cuenta_Joven():
-    def __init__(self,Cuenta, Bonificacion):
-        super.__init__(Cuenta)
+    def __init__(self, titular, saldo, movimientos, Bonificacion):
+        super.__init__( titular, saldo, movimientos)
         self.Bonificacion = Bonificacion
